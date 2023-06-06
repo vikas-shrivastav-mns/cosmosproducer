0a68d0def628ae0567f458da9ff763168e9d5040
Vikas Shrivastav <vikas-shrivastav-mns>
2023-06-06 13:58:19 +0000

    Initial commit

---
+++ src/main/kotlin/com/mns/Application.kt
@@ -0,0 +1,7 @@
+package com.mns
+
+import io.micronaut.runtime.Micronaut.run
+fun main(args: Array<String>) {
+	run(*args)
+}
+
---
+++ src/test/kotlin/com/mns/CosmosproducerTest.kt
@@ -0,0 +1,19 @@
+package com.mns
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class CosmosproducerTest {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
